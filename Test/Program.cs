b83eeccfbe87209f13eb874cb18cb2e409378d07
sunkaixuan <610262374@qq.com>
2015-05-15 14:31:54 +0800

    add english description

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sqlite.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,70 +11,127 @@ namespace Easyliter
     {
         static void Main(string[] args)
         {
+
             var connstr = "DataSource=" + System.AppDomain.CurrentDomain.BaseDirectory + "mapping.sqllite";
 
-            //依赖于 System.Data.SQLite
+            //reference System.Data.SQLite
+            //引用 System.Data.SQLite
             Easyliter e = new Easyliter(connstr);
 
-            //从数据库创建类
+            //Generate entity classes from a database
+            //从数据库生成实体类
+            CreateClassFile(e);
+
+            //Delete operation
+            //删除操作
+            DeleteData(e);
+
+            //update operation
+            //更新操作
+            UpdateData(e);
+
+            //insert operation
+            //插入数据
+            InserData(e);
+
+            //Search operation
+            //查询操作
+            Search(e);
+
+            //基本操作
+            //Basic operation
+            BasicOperation(e);
+        }
+
+
+
+
+        //search operation
+        //查询操作
+        private static void Search(Easyliter e)
+        {
+            ////By sql
+            List<Product> list = e.Select<Product>("select * from product where  id>@num", new { num = 100 });
+
+            //No parameter
+            List<Category> list2 = e.Select<Category>();
+
+            //Single parameter
+            List<Product> list3 = e.Select<Product>(x => x.id > 200);
+
+            //Multiple parameter
+            List<Product> list4 = e.Select<Product>(x => x.id > 200,
+                                                    x => x.sku == "skx" || x.sku == null);
+            //By page
+            int count = 0;
+            List<Product> list5 = e.SelectPage<Product>(1, 10, ref count, " id  desc",
+                                            x => x.id > 10,//条件1
+                                            x => true);//条件2 ...条件N
+
+
+            //Query extenions
+            var queryable = e.Query<Product>().Where(x => x.id > 10).Where(x => x.id > 2).Select("id,sku")
+                .OrderBy(El_Sort.asc, "id")
+                .OrderBy(El_Sort.desc, "sku").Take(100);
+
+            //get list
+            var list6 = queryable.ToList();
+            //get sql
+            string sql = queryable.ToSql();
+        }
+
+
+        //insert operation
+        //插入数据
+        private static void InserData(Easyliter e)
+        {
+            //添加
+            Product p = new Product()
+            {
+                category_id = 2,
+                sku = "sku",
+                title = "title"
+            };
+            e.Insert<Product>(p);
+        }
+
+
+        //update operation
+        //更新操作
+        private static void UpdateData(Easyliter e)
+        {
+            e.Update<Product>(new { sku = "AGA123101", category_id = 1 } /*update columns*/, new { id = 434 }/*where columns*/);
+        }
+
+
+        //Delete operation
+        //删除操作
+        private static void DeleteData(Easyliter e)
+        {
+            e.Delete<Product>(100);//primary key
+            e.Delete<Product>(new int[] { 1, 2, 3 });
+        }
+
+        //Generate entity classes from a database
+        //从数据库生成实体类
+        private static void CreateClassFile(Easyliter e)
+        {
+            //by database
             var createCalss1 = e.CreateClass("Sqlite.Model"/*命名空间*/, @"D:\TFS\EmailBackup\Easyliter\Test\model"/*路径*/);
 
-            ////根据SQL语句创建类,多表查询会用到
-            //var createCalss2 = e.CreateClassBySql("Sqlite.Model", @"D:\TFS\EmailBackup\Easyliter\Test\model1", "viewproduct", "select id,sku from product ");
+            
+            //by sql
+            var createCalss2 = e.CreateClassBySql("Sqlite.Model", @"D:\TFS\EmailBackup\Easyliter\Test\model1", "viewproduct", "select id,sku from product ");
+        }
 
-            ////删除
-            //// e.Delete<Product>(100);
-            //// e.Delete<Product>(new int [] {1,2,3});
-
-            ////更新
-            ////e.Update<Product>(new { sku = "x2",category_id=1 }, new { id=434 });
-
-
-            //////添加
-            ////Product p = new Product()
-            ////{
-            ////    category_id = 2,
-            ////    sku = "sku",
-            ////    title = "title"
-            ////};
-            ////e.Insert<Product>(p);
-
-            //////根据sql查询
-            //List<Product> list = e.Select<Product>("select * from product where  id>@num", new { num = 100 });
-
-            ////无条件
-            //List<Category> list2 = e.Select<Category>();
-
-            ////单条件
-            //List<Product> list3 = e.Select<Product>(x => x.id > 200);
-
-            ////多条件
-            //List<Product> list4 = e.Select<Product>(x => x.id > 200,
-            //                                        x => x.sku == "skx" || x.sku == null);
-            ////多条件分页
-            //int count = 0;
-            //List<Product> list5 = e.SelectPage<Product>(1, 10, ref count, " id  desc",
-            //                                x => x.id > 10,//条件1
-            //                                x => true);//条件2 ...条件N
-
-
-            ////linq语法生成集合
-            //var queryable = e.Query<Product>().Where(x => x.id > 10).Where(x => x.id > 2).Select("id,sku")
-            //    .OrderBy(El_Sort.asc, "id")
-            //    .OrderBy(El_Sort.desc, "sku").Take(100);
-
-            ////获取集合
-            //var list6 = queryable.ToList();
-            ////获取SQL
-            //string sql = queryable.ToSql();
-
-
-
-            ////原始操作
-            //var dt = e.GetDataTable("select * from product");
-            //var intVal = e.GetInt("select count(*) from product");
-            //var stringVal = e.GetString("select sku from product where id=500 ");
-            ////e.ExecuteNonQuery("inset into ..");
+        //基本操作
+        //Basic operation
+        private static void BasicOperation(Easyliter e)
+        {
+            var dt = e.GetDataTable("select * from product");
+            var intVal = e.GetInt("select count(*) from product");
+            var stringVal = e.GetString("select sku from product where id=500 ");
+            //e.ExecuteNonQuery("inset into ..");
         }
     }
 }
